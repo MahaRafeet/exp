46fcdcf26236f9e78c878ebfdcee063cb4a48db6
Maha <mahathamood98@gmail.com>
2025-08-30 16:32:35 +0400

    Adding comment to the code

--- LogicOperators.java
+++ LogicOperators.java
@@ -7,9 +7,11 @@ public class LogicOperators {
         Scanner scanner = new Scanner (System.in);
         System.out.print("Enter the user name :");
         String userName=scanner.nextLine();
+        //check the length of the user name
         if(userName.length()<4 || userName.length()>25) {
             System.out.println("The userName length must be more than 4 and less than 25 char ");
         }
+        //check the spaces and underscore 
             if (userName.contains("_") || userName.contains(" ")){
                 System.out.println("User name should not contains underscore or spaces ");
             }
