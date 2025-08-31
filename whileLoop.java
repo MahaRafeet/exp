9e08975786343b9974fbd2a70a0af43bb31b4569
Maha <mahathamood98@gmail.com>
2025-08-31 23:10:55 +0400

    Adding comments

--- whileLoop.java
+++ whileLoop.java
@@ -4,7 +4,7 @@ public class whileLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String response = "";
-
+//while the response is not Q continue the loop 
         while (!response.equals("Q")) {
             System.out.print("you are playing game , Enter Q to quite :");
             response = scanner.next().toUpperCase();
