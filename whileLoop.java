fade59c1cd4ac7117b7b006c1dd2f352f13bda53
Maha <mahathamood98@gmail.com>
2025-08-31 23:12:50 +0400

    Editing the code

--- whileLoop.java
+++ whileLoop.java
@@ -4,11 +4,13 @@ public class whileLoop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String response = "";
-//while the response is not Q continue the loop 
+//while the response is not Q continue the loop
         while (!response.equals("Q")) {
             System.out.print("you are playing game , Enter Q to quite :");
+            //use toUpperCase in case the user inter lowerCase Q
             response = scanner.next().toUpperCase();
         }
+
         System.out.println("YOU Quite the game ");
     }
 }
