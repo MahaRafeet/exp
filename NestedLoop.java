f853cabf7fe1edc8f4bfbdc24a8ee99a3b4ceb61
Maha <mahathamood98@gmail.com>
2025-09-01 23:33:25 +0400

    Adding nested loop file

---
+++ NestedLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class NestedLoop {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner (System.in);
+        System.out.print("Enter number of rows: ");
+        int row = scanner.nextInt();
+        System.out.print("Enter number of columns: ");
+        int columns = scanner.nextInt();
+        System.out.print("Enter Symbol to use: ");
+        char symbol = scanner.next().charAt(0);
+        for(int i= 0 ; i<row;i++){
+            for(int j =0 ; j<columns ;j++){
+                System.out.print(symbol);
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
