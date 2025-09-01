ac845091a5ecd5277a294acab95ece9e28336658
Maha <mahathamood98@gmail.com>
2025-09-01 19:36:09 +0400

    Adding comments to the code

--- GuessingGame.java
+++ GuessingGame.java
@@ -3,12 +3,14 @@ import java.util.Random;
 public class GuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Random object to creat random number
         Random random = new Random();
         int guess = 0;
         int attempt=0;
         int randomNumber = random.nextInt(1, 11);
         System.out.println("Number Guessing Game");
         System.out.println("Guess a Number between 1 and 10 ");
+        //do-while loop
         do{
             System.out.print("Enter a guess : ");
             guess =scanner.nextInt();
@@ -25,9 +27,6 @@ public class GuessingGame {
                 System.out.println(" The correct number is  "+ randomNumber);;
 
             }
-
-
-
         }while(guess !=randomNumber);
         scanner.close();
     }
